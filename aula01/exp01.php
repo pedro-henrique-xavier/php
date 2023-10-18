7195cb54785c3b1d3cdc977ab8ee8e6d2dbab384
Pedro Xavier <145489032+pedro-henrique-xavier@users.noreply.github.com>
2023-10-18 14:00:48 -0300

    teste de alteração

--- aula01/exp01.php
+++ aula01/exp01.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Exp01</title>
+    <title>Teste 1</title>
 </head>
 <body>
+    <h1>Teste alterção git</h1>
     <?php
-    echo "arquivo em php";
+    echo "teste 1";
     ?>
+    
 
 </body>
 </html>
